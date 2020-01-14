cfce546d7cec9c1a16153923cf73d920f7fc73dd
Hrant-p <hrant@live.ru>
2020-01-14 19:28:59 +0400

    refactoring Table class component to functional component, using Hooks

--- src/container/Sports/Sports.js
+++ src/container/Sports/Sports.js
@@ -1,34 +1,31 @@
-import React, { Fragment } from 'react';
-import Table from '../Table/Table';
+import React from 'react';
 import { connect } from 'react-redux';
-import {errorSelector, isLoadingSelector} from '../../store/selectors/sportSelector';
-import Spinner from "../../components/Spinner/Spinner";
-import Error from "../Error/Error";
-import {withRouter} from "react-router";
+import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
+import Table from '../Table/Table';
+import { errorSelector, isLoadingSelector } from '../../store/selectors/sportSelector';
+import Spinner from '../../components/Spinner/Spinner';
+import Error from '../Error/Error';
 import './Sports.scss';
 
-function Sports(props) {
-    const {error, isLoading} = props;
-    return (
-        <Fragment>
-            {error && <Error/>}
-            {isLoading && <Spinner/>}
-            <div className="sports-container">
-                <Table/>
-            </div>
-        </Fragment>
-    );
-}
+const Sports = ({ error, isLoading }) => (
+  <>
+    {error && <Error />}
+    {isLoading && <Spinner />}
+    <div className="sports-container">
+      <Table />
+    </div>
+  </>
+);
 
 const mapStateToPops = state => ({
-    isLoading: isLoadingSelector(state),
-    error: errorSelector(state)
+  isLoading: isLoadingSelector(state),
+  error: errorSelector(state),
 });
 
 Sports.propTypes = {
-    isLoading: PropTypes.bool,
-    error: PropTypes.string
+  isLoading: PropTypes.bool,
+  error: PropTypes.string,
 };
 
 export default connect(mapStateToPops, null)(withRouter(Sports));
--- src/container/Table/Table.js
+++ src/container/Table/Table.js
@@ -1,119 +1,126 @@
-import React, {Component} from 'react';
-import Tab from "../../components/Tab/Tab";
-import AllSportTabs from "../../components/AllSportTabs/AllSportTabs";
-import {connect} from "react-redux";
-import {
-    basketballSelector,
-    errorSelector,
-    footballSelector,
-    isLoadingSelector,
-    rugbySelector,
-    sportTypeSelector,
-    valleyballSelector
-} from "../../store/selectors/sportSelector";
-import {bindActionCreators} from "redux";
-import {
-    getBasketballRequest,
-    getFootballRequest,
-    getRugbyRequest,
-    getValleyballRequest
-} from "../../store/actions/sportActionCreators";
-import {withRouter} from "react-router";
-import {countryId} from "../../API/apiFootball";
-import {filterListByCountry} from "../../helpers";
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { useHistory, useParams } from 'react-router';
 import PropTypes from 'prop-types';
-import './Table.scss'
-import {uid} from "react-uid";
+import { uid } from 'react-uid';
+import Tab from '../../components/Tab/Tab';
+import AllSportTabs from '../../components/AllSportTabs/AllSportTabs';
+import {
+  basketballSelector,
+  errorSelector,
+  footballSelector,
+  isLoadingSelector,
+  rugbySelector,
+  sportTypeSelector,
+  valleyballSelector,
+} from '../../store/selectors/sportSelector';
+import {
+  getBasketballRequest,
+  getFootballRequest,
+  getRugbyRequest,
+  getValleyballRequest,
+} from '../../store/actions/sportActionCreators';
+import { countryId } from '../../API/apiFootball';
+import { filterListByCountry } from '../../helpers';
+import './Table.scss';
 
-class Table extends Component {
-    componentDidMount() {
-        const {
-            currentPageSport,
-            getFootballRequest,
-            getBasketballRequest,
-            getValleyballRequest,
-            getRugbyRequest,
-            rugby,
-            basketball,
-            valleyball,
-            history,
-            match : { params: {type}}
-        } = this.props;
+const Table = props => {
+  const {
+    currentPageSport,
+    getFootballRequest,
+    getBasketballRequest,
+    getValleyballRequest,
+    getRugbyRequest,
+    rugby,
+    basketball,
+    valleyball,
+    error,
+  } = props;
+  const history = useHistory();
+  const { type } = useParams();
 
-        if (window.location.pathname.includes('/sports/') && !type) {
-            history.push('/sports/football')
-        }
-
-        if (type === 'football') {
-            getFootballRequest([]);
-        } else if (type === 'basketball' && !basketball.size && currentPageSport !== 'basketball') {
-            getBasketballRequest([]);
-        } else if (type === 'valleyball' && !valleyball.size && currentPageSport !== 'valleyball') {
-            getValleyballRequest([])
-        } else if (type === 'rugby' && !rugby.size && currentPageSport !== 'rugby') {
-            getRugbyRequest([]);
-        }
-
-    };
-
-    drawTableBody = sportMap => {
-        const countryNames = Object.keys(countryId);
-        return countryNames.map(country => <Tab
-            key={uid(country)}
-            label={country}
-            data={sportMap ? filterListByCountry(sportMap, country) : []}
-            error={this.props.error}
-        />);
-    };
-
-    render() {
-        const { currentPageSport, error } = this.props;
-        const selectedState = this.props[currentPageSport];
-        return (
-            <div className="table-container">
-                <AllSportTabs/>
-                {!error &&
-                <div className="tbody">
-                    {this.drawTableBody(selectedState)}
-                </div>}
-            </div>
-        );
+  useEffect(() => {
+    if (window.location.pathname.includes('/sports/') && !type) {
+      history.push('/sports/football');
     }
-}
+
+    if (type === 'football') {
+      getFootballRequest([]);
+    } else if (type === 'basketball' && !basketball.size && currentPageSport !== 'basketball') {
+      getBasketballRequest([]);
+    } else if (type === 'valleyball' && !valleyball.size && currentPageSport !== 'valleyball') {
+      getValleyballRequest([]);
+    } else if (type === 'rugby' && !rugby.size && currentPageSport !== 'rugby') {
+      getRugbyRequest([]);
+    }
+  }, [
+    currentPageSport,
+    getFootballRequest,
+    getBasketballRequest,
+    getValleyballRequest,
+    getRugbyRequest,
+    rugby,
+    basketball,
+    valleyball,
+    history,
+    type,
+  ]);
+
+  const countryNames = Object.keys(countryId);
+  const drawTableBody = selectedState => countryNames.map(country => (
+    <Tab
+      key={uid(country)}
+      label={country}
+      data={selectedState ? filterListByCountry(selectedState, country) : []}
+      error={error}
+    />
+  ));
+
+  const selectedState = props[currentPageSport];
+  return (
+    <div className="table-container">
+      <AllSportTabs />
+      <div className="tbody">
+        {drawTableBody(selectedState)}
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
-    currentPageSport: sportTypeSelector(state),
-    football: footballSelector(state),
-    basketball: basketballSelector(state),
-    rugby: rugbySelector(state),
-    valleyball: valleyballSelector(state),
-    isLoading: isLoadingSelector(state),
-    error: errorSelector(state)
+  currentPageSport: sportTypeSelector(state),
+  football: footballSelector(state),
+  basketball: basketballSelector(state),
+  rugby: rugbySelector(state),
+  valleyball: valleyballSelector(state),
+  isLoading: isLoadingSelector(state),
+  error: errorSelector(state),
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators(
-    {
-        getFootballRequest,
-        getBasketballRequest,
-        getValleyballRequest,
-        getRugbyRequest
-    },
-    dispatch
+  {
+    getFootballRequest,
+    getBasketballRequest,
+    getValleyballRequest,
+    getRugbyRequest,
+  },
+  dispatch,
 );
 
 Table.propTypes = {
-    currentPageSport: PropTypes.string,
-    getFootballRequest: PropTypes.func,
-    getBasketballRequest: PropTypes.func,
-    getValleyballRequest: PropTypes.func,
-    getRugbyRequest: PropTypes.func,
-    rugby: PropTypes.object,
-    basketball: PropTypes.object,
-    valleyball: PropTypes.object,
-    isLoading: PropTypes.bool,
-    error: PropTypes.string,
+  currentPageSport: PropTypes.string,
+  getFootballRequest: PropTypes.func,
+  getBasketballRequest: PropTypes.func,
+  getValleyballRequest: PropTypes.func,
+  getRugbyRequest: PropTypes.func,
+  rugby: PropTypes.object,
+  basketball: PropTypes.object,
+  valleyball: PropTypes.object,
+  isLoading: PropTypes.bool,
+  error: PropTypes.string,
 };
 
 export default connect(
-    mapStateToProps, mapDispatchToProps
-)(withRouter(Table));
+  mapStateToProps, mapDispatchToProps,
+)(Table);
